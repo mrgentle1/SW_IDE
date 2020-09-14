8bc6d9d416c45506bf33f59473eb9aa61c839b07
jun <59019322+sucker15@users.noreply.github.com>
2020-09-14 11:41:11 +0900

    Add files via upload
    Add 05_practice_2.ino file

---
+++ 05_practice_2.ino
@@ -0,0 +1,21 @@
+#define PIN_LED 7
+unsigned int toggle;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  toggle = 0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+  for (int i=0;i<11;i++){
+    toggle = !toggle;
+    digitalWrite(PIN_LED, toggle);
+    delay(200);
+  }
+  while(1){}
+}
